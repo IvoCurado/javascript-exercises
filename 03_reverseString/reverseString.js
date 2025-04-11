1d743e2c3d258c82dd992213d3879a52626bd4b7
Ivo Curado <ivocurado@live.com.pt>
2025-04-11 18:48:47 +0100

    feat: Implement Reverse text

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function (textToReverse) {
+  let textToReverseChars = textToReverse.split("");
+  let reversedTextChars = [];
+  for (let i = textToReverseChars.length - 1; i >= 0; i--) {
+    reversedTextChars.push(textToReverseChars[i]);
+  }
+  let reversedText = reversedTextChars.join('');
+  return reversedText;
 };
 
 // Do not edit below this line
