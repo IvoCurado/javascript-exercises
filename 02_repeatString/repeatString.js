2d358ee757cf9d1dd789a30413afc2b4ef1b9348
Ivo Curado <ivocurado@live.com.pt>
2025-04-11 18:40:11 +0100

    feat: Implement Repeat text function

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function (textToRepeat, numberOfTimes) {
+  if (numberOfTimes < 0) {
+    return "ERROR";
+  }
+  let result = "";
+  for (let i = 0; i < numberOfTimes; i++) {
+    result += textToRepeat;
+  }
+  return result;
 };
 
 // Do not edit below this line
