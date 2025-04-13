dbe41d8aeb82f10b4a1fe9701d85f907a804eb1b
Ivo Curado <ivocurado@live.com.pt>
2025-04-13 18:37:41 +0100

    feat: Calculate temperatures conversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,19 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (fahrenheitValue) {
+  let convertedTemperature = ((fahrenheitValue - 32) * 5) / 9;
+  return Number.isInteger(convertedTemperature)
+    ? convertedTemperature
+    : convertedTemperature.toFixed(1) - 0;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (celsiusValue) {
+  let convertedTemperature = celsiusValue * 1.8 + 32;
+  return Number.isInteger(convertedTemperature)
+    ? convertedTemperature
+    : convertedTemperature.toFixed(1) - 0;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
