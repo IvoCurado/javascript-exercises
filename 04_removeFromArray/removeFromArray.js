e7adcdf4f91334eadc6645ccdcf9d755f1f478ad
Ivo Curado <ivocurado@live.com.pt>
2025-04-13 18:12:56 +0100

    feat: Implement remove from array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,6 @@
-const removeFromArray = function() {
+const removeFromArray = function(arrayToProcess, ...itemsToRemove) {
+    let processedArray = arrayToProcess.filter((element) => !itemsToRemove.includes(element));
+    return processedArray;
 };
 
 // Do not edit below this line
